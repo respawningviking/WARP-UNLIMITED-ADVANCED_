2546fa23c7fc6644b20b11d53fc8115c142d453a
respawningviking <bsrnberk@gmail.com>
2024-10-12 13:37:52 +0300

    Update warp.py

--- warp.py
+++ warp.py
@@ -16,7 +16,8 @@ filterwarnings("ignore", category=DeprecationWarning)
 os.system("title WARP UNLIMITED ADVANCED")
 os.system("cls" if os.name == "nt" else "clear")
 
-referrer = input("[#] Enter the WARP+ ID:\n")
+referrer = b51ec48b-d5e8-45fe-bb4a-a726db39f89b
+
 
 g = 0
 b = 0
